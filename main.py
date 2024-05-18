c2f4126e34c808ac31757a93ed0d7e90f0ca8d08
Kglebowicz <63111150+Kglebowicz@users.noreply.github.com>
2024-05-18 10:22:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import os
 import pyodbc
-import random
+from random import randint
 from PIL import Image
 from tkinter import Tk, Label
 from PIL import Image, ImageTk
+import os
 from tabulate import tabulate
+import time
+
 PoUtworzeniu = 0
 
 #v.01
@@ -12,6 +14,8 @@ PoUtworzeniu = 0
 
 #v.02
 #Utworzono uruchamianie gry, wyswietlanie planszy i informacji o grze, dodano mozliwosc wykonania rzutu kostka
+#v.03
+#Utworzono mechanike gry po stronie bazy danych oraz mozliwosc pelnego ruchu gracza,dodano multiplayer
 def display_image(image_path):
    
     image = Image.open(image_path)
@@ -65,7 +69,6 @@ def print_tables(cursor,gameroom_name):
             row.extend([""] * len(nieruchomosci_table[0]))
         
         combined_table.append(row)
-    print("#################Gracze###############|####Miasta####|")
     print(tabulate(combined_table, tablefmt="plain"))
 def start_or_join_game(gameroom_name,player):
     PoUtworzeniu = 0
@@ -81,11 +84,71 @@ def start_or_join_game(gameroom_name,player):
 
     cursor.execute("SELECT Nazwa FROM ListaGier WHERE Nazwa = ?", gameroom_name)
     existing_room = cursor.fetchone()
-
+    
     if existing_room:
 
         print("Dolaczam do pokoju:", gameroom_name)
         print(existing_room)
+        connection.commit() 
+        image_path = "C:/Users/user/Desktop/Monopoly/plansza1.png"  
+        display_image(image_path)
+        print('Rozpoczynam gre')
+        os.system('cls')
+        print_tables(cursor,gameroom_name)
+        CzyBankrut = 0
+        CzyRuch = 1
+        while CzyBankrut == 0 and CzyRuch == 1:
+            ruch = input('Wcisknij dowolny guzik zeby rzucic kostka ')
+            wynik = randint(1,6)
+
+                
+  
+            cursor.execute("EXEC RuchGracza @wynik = ?, @gracz = ?, @nazwaPokoju = ?", (wynik,player,gameroom_name))
+            cursor.execute("SELECT AktualnePole FROM Gracze WHERE nazwaPokoju = ? and NickGracza = ?", (gameroom_name, player))
+            pole = cursor.fetchone().AktualnePole
+            os.system('cls')
+            print_tables(cursor,gameroom_name)
+            print('Wyrzuciles: ' + str(wynik))
+            print('Aktualnie znajdujesz się na polu: ' + str(pole))
+            cursor.execute("SELECT odpowiedz, komunikat FROM Komunikaty where nazwaPokoju = ? and Gracz = ?", (gameroom_name, player))
+            result = cursor.fetchone()
+            odpowiedz = result.odpowiedz
+            komunikat = result.komunikat
+                
+            print(komunikat)
+            if odpowiedz == 'Bankrut':
+                image_path1 = "C:/Users/user/Desktop/Monopoly/bankrut.jpg"  
+                display_image(image_path1)
+                exit()
+                CzyBankrut = 1
+            if odpowiedz == "WolneMiasto":
+                wybor = input('Wybierz: ')
+                if wybor == '1':
+                    cursor.execute("EXEC ZakupMiasta @nazwaPokoju = ?", (player, gameroom_name))
+                    print('Zakupiono miasto.')
+                else:
+                    cursor.execute("EXEC ZmianaKolejki @nazwaPokoju = ?", (gameroom_name))
+                    CzyRuch = 0
+            if odpowiedz == "WolnaNieruchomosc":
+                wybor = input('Wybierz: ')
+                if wybor == '1':
+                    cursor.execute("EXEC ZakupNieruchomosci @nazwaPokoju = ?", (player, gameroom_name))
+                    print('Zakupiono nieruchomosc.')
+                else:
+                    cursor.execute("EXEC ZmianaKolejki @nazwaPokoju =?", (gameroom_name))
+                    CzyRuch = 0
+            else:
+                    cursor.execute("EXEC ZmianaKolejki @nazwaPokoju = ?", (gameroom_name))
+                    CzyRuch = 0 
+        while CzyBankrut == 0 and CzyRuch == 0:
+            time.sleep(5)
+            os.system('cls')
+            print_tables(cursor,gameroom_name)
+            print('Koniec twojej rundy')
+            cursor.execute("SELECT NickGracza from Gracze a join ListaGier b on a.id  = b.RuchGracza and a.nazwaPokoju = b.nazwa where b.nazwa = ?", (gameroom_name))
+            CheckRuchu = cursor.fetchone()
+            if CheckRuchu == str(player):
+                CzyRuch = 1
     else:
 
         print("Tworze nowy Pokoj:", gameroom_name)
@@ -94,6 +157,7 @@ def start_or_join_game(gameroom_name,player):
         while PoUtworzeniu != '2':
             cursor.execute("SELECT gracze FROM ListaGier WHERE Nazwa = ?", gameroom_name)
             players = cursor.fetchone()
+            os.system('cls')
             print("Aktualny pokoj:", existing_room)
             print("Gracze: ", players)
             print('[1] Odswiez liste graczy')
@@ -103,22 +167,70 @@ def start_or_join_game(gameroom_name,player):
             ruch = 0
             cursor.execute("INSERT INTO Gracze (nazwaPokoju, NickGracza) values(?, ?)", (gameroom_name, player))
             cursor.execute('EXEC InsertMiasta @nazwapokoju = ? ', gameroom_name)
+            cursor.execute('UPDATE a  SET a.RuchGracza = b.id  FROM ListaGier a JOIN (SELECT id, nazwaPokoju from Gracze where Nickgracza  = ? and nazwaPokoju = ? ) b on a.nazwa = b.nazwaPokoju', (player, gameroom_name))
             connection.commit() 
-            os.system('cls')
+            image_path = "C:/Users/user/Desktop/Monopoly/plansza1.png"  
             display_image(image_path)
             print('Rozpoczynam gre')
+            os.system('cls')
             print_tables(cursor,gameroom_name)
             CzyBankrut = 0
-            CzyRuch = 0
+            CzyRuch = 1
             while CzyBankrut == 0 and CzyRuch == 1:
                 ruch = input('Wcisknij dowolny guzik zeby rzucic kostka ')
-                wynik = random(1,6)
-                print('Wyrzuciles: ' + wynik)
-                cursor.execute("SELECT AktualnePole FROM Gracze WHERE nazwaPokoju = ? and NickGracza = ?", (gameroom_name, player))
-                pole = cursor.fetchone()
-                pole = pole + wynik
+                wynik = randint(1,6)
+
+                
+  
                 cursor.execute("EXEC RuchGracza @wynik = ?, @gracz = ?, @nazwaPokoju = ?", (wynik,player,gameroom_name))
-            
+                cursor.execute("SELECT AktualnePole FROM Gracze WHERE nazwaPokoju = ? and NickGracza = ?", (gameroom_name, player))
+                pole = cursor.fetchone().AktualnePole
+                os.system('cls')
+                print_tables(cursor,gameroom_name)
+                print('Wyrzuciles: ' + str(wynik))
+                print('Aktualnie znajdujesz się na polu: ' + str(pole))
+                cursor.execute("SELECT odpowiedz, komunikat FROM Komunikaty where nazwaPokoju = ? and Gracz = ?", (gameroom_name, player))
+                result = cursor.fetchone()
+                odpowiedz = result.odpowiedz
+                komunikat = result.komunikat
+                
+                print(komunikat)
+                if odpowiedz == 'Bankrut':
+                    image_path1 = "C:/Users/user/Desktop/Monopoly/bankrut.jpg"  
+                    display_image(image_path1)
+                    exit()
+                    CzyBankrut = 1
+                if odpowiedz == "WolneMiasto":
+                    wybor = input('Wybierz: ')
+                    if wybor == '1':
+                        cursor.execute("EXEC ZakupMiasta @nazwaPokoju = ?", (player, gameroom_name))
+                        print('Zakupiono miasto.')
+                    else:
+                        cursor.execute("EXEC ZmianaKolejki @nazwaPokoju = ?", (gameroom_name))
+                        CzyRuch = 0
+                if odpowiedz == "WolnaNieruchomosc":
+                    wybor = input('Wybierz: ')
+                    if wybor == '1':
+                        cursor.execute("EXEC ZakupNieruchomosci @nazwaPokoju = ?", (player, gameroom_name))
+                        print('Zakupiono nieruchomosc.')
+                    else:
+                        cursor.execute("EXEC ZmianaKolejki @nazwaPokoju =?", (gameroom_name))
+                        CzyRuch = 0
+                else:
+                        cursor.execute("EXEC ZmianaKolejki @nazwaPokoju = ?", (gameroom_name))
+                        CzyRuch = 0 
+            while CzyBankrut == 0 and CzyRuch == 0:
+                time.sleep(5)
+                os.system('cls')
+                print_tables(cursor,gameroom_name)
+                print('Koniec twojej rundy')
+                cursor.execute("SELECT NickGracza from Gracze a join ListaGier b on a.id  = b.RuchGracza and a.nazwaPokoju = b.nazwa where b.nazwa = ?", (gameroom_name))
+                CheckRuchu = cursor.fetchone()
+                if CheckRuchu == str(player):
+                    CzyRuch = 1
+
+                
+                
 
     connection.close()
 
@@ -126,6 +238,6 @@ if __name__ == "__main__":
     
     player = input("Podaj swoj Nick: ")
     gameroom_name = input("Podaj nazwe pokoju: ")
-    image_path = "C:/Users/user/Desktop/Monopoly/plansza1.png"  
+    
     
     start_or_join_game(gameroom_name, player)
